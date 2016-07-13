e00c015b23da5b1fa4c949c1495914590e7c983b
Shashank Gupta <27392shashankgupta@gmail.com>
2016-07-14 03:11:29 +0530

    preprocessing updated

--- preprocessing_keras.py
+++ preprocessing_keras.py
@@ -20,7 +20,7 @@ class preprocess(object):
         # Preprocessing hindi corpus
 
         self.text_hindi =[line.rstrip() for line in open(self.path_hindi).readlines()]
-        #text_eng = [line.rstrip() for line in codecs.open(self.path_eng, encoding='utf-8').readlines()]
+        #self.text_eng = [line.rstrip() for line in codecs.open(self.path_eng, encoding='utf-8').readlines()]
         self.text_eng = [line.rstrip() for line in open(self.path_eng).readlines()]
 
         self.tokenizer = Tokenizer(self.max_feat)
@@ -28,31 +28,19 @@ class preprocess(object):
         self.vocab_en = self.tokenizer.word_index
         self.vocab_en["UNK"] = self.max_feat
         self.vocab_en["<s>"] = self.max_feat + 1
-        self.vocab_en["<b>"] = self.max_feat + 2
+        self.vocab_en["</s>"] = self.max_feat + 2
         self.tokenizer1 = Tokenizer(self.max_feat)
         self.tokenizer1.fit_on_texts(self.text_hindi)
         self.vocab_hind = self.tokenizer1.word_index
         self.vocab_hind["UNK"] = self.max_feat
         self.vocab_hind["<s>"] = self.max_feat + 1
-        self.vocab_hind["<b>"] = self.max_feat + 2
+        self.vocab_hind["</s>"] = self.max_feat + 2
         text1 = self.tokenizer.texts_to_sequences(self.text_eng)
         text2 = self.tokenizer1.texts_to_sequences(self.text_hindi)
         print text1[:6]
         print text2[:6]
 
-    def gen_seq(self, batch_size=64):
-        i = 0
-        text_seq = []
-        text_seq1 = []
-        for text1, text2 in zip(self.tokenizer.texts_to_sequences_generator(self.text_eng), self.tokenizer1.texts_to_sequences_generator(self.text_hindi)):
-            if i == batch_size:
-                break
-            text_seq.append(text1)
-            text_seq1.append(text2)
-            i += 1
-        #pdb.set_trace()
-
-        # Filter out non-zero sequences (sequences with words having less freq)
+    def gen_seq(self, text_seq, text_seq1):
         nonzero_ind = []
         for ind, seq in enumerate(zip(text_seq, text_seq1)):
             if len(seq[0]) !=0 and len(seq[1]) !=0:
@@ -65,52 +53,77 @@ class preprocess(object):
         # Normalize all sequences to maxlen
         #X = pad_sequences(text_seq_X, self.max_len)
         #Y = pad_sequences(text_seq_Y, self.max_len)
-        X = np.zeros((len(text_seq_X), self.max_len + 2))
-        Y = np.zeros((len(text_seq_Y), self.max_len + 2, self.max_feat + 3), dtype=np.float16)
+        X = np.zeros((len(text_seq_X), self.max_len + 2), np.int32)
+        Y = np.zeros((len(text_seq_Y), self.max_len + 2, self.max_feat + 3), dtype=np.int32)
         #pdb.set_trace()
         for ind, seq in enumerate(zip(text_seq_X, text_seq_Y)):
-            if len(seq[0]) <= self.max_len:
-                pdb.set_trace()
+            if len(seq[0]) <= (self.max_len):
                 X[ind, 0] = self.vocab_en["<s>"]
                 X[ind, 1:len(seq[0])+1] = seq[0]
-                X[ind, len(seq[0])+1:] = self.vocab_en["<b>"]
+                X[ind, len(seq[0])+1:] = self.vocab_en["</s>"]
 
-            elif len(seq[0]) > self.max_len :
+            elif len(seq[0]) > self.max_len:
                 if self.truncate == 'post':
-                    temp = seq[:self.max_len-2]
+                    temp = seq[0][:self.max_len]
                     X[ind, 0] = self.vocab_en["<s>"]
                     X[ind, 1:len(temp)+1] = temp
-                    X[ind, len(seq[0])+1:] = self.vocab_en["<b>"]
+                    X[ind, len(temp)+1:] = self.vocab_en["</s>"]
                 else:
-                    temp = self[-(self.max_len -2):]
+                    temp = self[0][-self.max_len:]
                     X[ind, 0] = self.vocab_en["<s>"]
-                    X[ind, 1:len(temp)] = temp
-                    X[ind, len(seq[0]):] = self.vocab_en["<b>"]
+                    X[ind, 1:(len(temp) + 1)] = temp
+                    X[ind, (len(temp) + 1):] = self.vocab_en["</s>"]
 
-            if len(seq[1]) <= self.max_len -2 :
+            #pdb.set_trace()
+            if len(seq[1]) <= self.max_len:
                 Y[ind, 0, self.vocab_en["<s>"]] = 1
+                # 2nd dim ind + 1 because ind1 starts with 0 and 0 is filled
+                # with <s>
                 for ind1,j in enumerate(seq[1]):
-                    Y[ind, ind1, j] = 1
-                for ind1 in xrange(len(seq[1]), self.max_len):
-                    Y[ind, ind1, self.vocab_en["<b>"]] = 1
+                    Y[ind, (ind1 + 1), j] = 1
 
-            elif len(seq[0]) > self.max_len - 2:
+                for ind1 in xrange(len(seq[1])+1, self.max_len+2):
+                    Y[ind, ind1, self.vocab_en["</s>"]] = 1
+
+            elif len(seq[1]) > self.max_len:
                 if self.truncate == 'post':
-                    temp = seq[:self.max_len-2]
-                else:
-                    temp = self[-(self.max_len -2):]
-                Y[ind, 0, self.vocab_en["<s>"]] = 1
-                for ind1,j in enumerate(seq[1]):
-                    Y[ind, ind1, j] = 1
-                for ind1 in xrange(len(seq[1]), self.max_len):
-                    Y[ind, ind1, self.vocab_en["<b>"]] = 1
+                    temp = seq[1][:self.max_len]
 
-        pdb.set_trace()
+                else:
+                    temp = seq[1][-self.max_len:]
+
+                Y[ind, 0, self.vocab_en["<s>"]] = 1
+
+                for ind1,j in enumerate(temp):
+                    Y[ind, (ind1 + 1), j] = 1
+
+                for ind1 in xrange(len(temp) + 1, self.max_len + 2):
+                    Y[ind, ind1, self.vocab_en["</s>"]] = 1
+
+            return X, Y
+
+
+    def gen_batch(self, batch_size=32):
+        i = 0
+        text_seq = []
+        text_seq1 = []
+        for text1, text2 in zip(self.tokenizer.texts_to_sequences_generator(self.text_eng), self.tokenizer1.texts_to_sequences_generator(self.text_hindi)):
+            if i == batch_size:
+                X, Y = self.gen_seq(text_seq, text_seq1)
+                text_seq = []
+                text_seq1 = []
+                i = 0
+                yield X, Y
+            text_seq.append(text1)
+            text_seq1.append(text2)
+            i += 1
+                #pdb.set_trace()
 
 
 
 if __name__ == "__main__":
     pre = preprocess('../indian-parallel-corpora/hi-en/tok/training.hi-en.hi', '../indian-parallel-corpora/hi-en/tok/training.hi-en.en', 5000, 50)
     pre.preprocess()
-    pre.gen_seq()
-
+    for X,Y in pre.gen_batch():
+        print X.shape, Y.shape
+    pdb.set_trace()
--- seq2seq_mt.py
+++ seq2seq_mt.py
@@ -6,7 +6,7 @@ from keras.layers.core import Activation, RepeatVector, TimeDistributedDense, Dr
 from keras.layers import recurrent
 from keras.layers.embeddings import Embedding
 import numpy as np
-from preprocess import preprocess
+from preprocessing_keras import preprocess
 import pdb
 RNN = recurrent.LSTM
 
@@ -14,10 +14,10 @@ class seq2seq(object):
     # Train a sequence to sequence model for hindi to english translation
     def __init__(self, maxlen, vocab_size):
         self.maxlen = maxlen
-       # self.batch = batch
+        self.vocab_size = vocab_size
         self.proproces = preprocess("../indian-parallel-corpora/hi-en/tok/training.hi-en.hi",
-                           "../indian-parallel-corpora/hi-en/tok/training.hi-en.en", maxlen, vocab_size)
-        self.proproces.gen_vocab()
+                           "../indian-parallel-corpora/hi-en/tok/training.hi-en.en", vocab_size, maxlen)
+        self.proproces.preprocess()
         #self.X_train = self.proproces.gen_seq_X("/home/shashank/datasets/MT-dataset/indian-parallel-corpora/hi-en/tok/training.hi-en.hi",
         #              "hindi")
         #self.Y_train = self.proproces.gen_seq_X("/home/shashank/datasets/MT-dataset/indian-parallel-corpora/hi-en/tok/training.hi-en.en",
@@ -36,7 +36,7 @@ class seq2seq(object):
         #np.random.shuffle(indices)
         #self.X_train = self.X_train[indices]
         #self.Y_train = self.Y_train[indices]
-    
+
     def decode(self, prob):
         # Decodes the probability tensor into english sentence
         n_sent = prob.shape[0]
@@ -44,62 +44,53 @@ class seq2seq(object):
             eng_ind = prob[sent_id].argmax(axis=-1)
             print eng_ind
             return ' '.join(self.proproces.vocab_en_rev[ind] for ind in eng_ind)
-    
+
     def train_seq2seq(self):
         print "Input sequence read, starting training"
-        #X_train = sequence.pad_sequences(self.X_train, maxlen=self.maxlen)
-        #Y_train = sequence.pad_sequences(self.Y_train, maxlen=self.maxlen)
-        #X_val = sequence.pad_sequences(self.X_val, maxlen=self.maxlen)
-        #y_val = sequence.pad_sequences(self.Y_val, maxlen=self.maxlen)
-        #X_test = sequence.pad_sequences(self.X_test, maxlen=self.maxlen)
-        #Y_test = sequence.pad_sequences(self.Y_test, maxlen=self.maxlen)
 
         model = Sequential()
-        model.add(Embedding(len(self.proproces.vocab_hind), 30,
-                            input_length=self.maxlen))
-        model.add(RNN(30))#, input_shape=(100, 128)))
-        model.add(RepeatVector(self.maxlen))
-        model.add(RNN(30, return_sequences=True))
-        model.add(TimeDistributedDense(len(self.proproces.vocab_en)))
+        model.add(Embedding(self.vocab_size + 3, 100))
+        model.add(RNN(100))#, input_shape=(100, 128)))
+        model.add(RepeatVector(self.maxlen + 2))
+        model.add(RNN(100, return_sequences=True))
+        model.add(TimeDistributedDense(self.vocab_size + 3))
         model.add(Activation('softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer='adam',
                       metrics=['accuracy'])
         for e in range(100):
             print("epoch %d" % e)
-            for (X,Y) in self.proproces.gen_seq("../indian-parallel-corpora/hi-en/tok/dev.hi-en.en.0",
-                                 "../indian-parallel-corpora/hi-en/tok/dev.hi-en.hi",
-                                           128):
+            for (X,Y) in self.proproces.gen_batch():
                 loss, acc = model.train_on_batch(X, Y)#, batch_size=64, nb_epoch=1)
                 print("Loss is %f, accuracy is %f " % (loss, acc))
             # After one epoch test one sentence
-            if e % 5 == 0 :
-                print("Enter sentence in hindi")
-                inp = raw_input().decode("utf-8")
-                tokens = inp.split()
-                seq = []
-                for token in tokens:
-                    if token in self.proproces.vocab_hind:
-                        seq.append(self.proproces.vocab_hind[token])
-                    else:
-                        token = "UNK"
-                        seq.append(self.proproces.vocab_hind[token])
-                #seq = map(lambda x:self.proproces.vocab_hind[x], tokens)
-                # Normalize seq to maxlen
-                X = []
-                X.append(seq)
-                print X
-                temp = sequence.pad_sequences(X, maxlen=self.maxlen)
-                #temp[0:len(seq)] = seq
-                print len(temp)
-                #temp = np.asarray(temp).reshape(128,)
-                print temp.shape
-                prob = model.predict_on_batch(temp)#, batch_size=1, verbose=0)
-                translated = self.decode(prob)
-                print("Tranlated is", translated)
-                print("Probabilities are", prob)
-                print("Shape of prob tensor is",prob.shape)
+            #if e % 5 == 0 :
+            #    print("Enter sentence in hindi")
+            #    inp = raw_input().decode("utf-8")
+            #    tokens = inp.split()
+            #    seq = []
+            #    for token in tokens:
+            #        if token in self.proproces.vocab_hind:
+            #            seq.append(self.proproces.vocab_hind[token])
+            #        else:
+            #            token = "UNK"
+            #            seq.append(self.proproces.vocab_hind[token])
+            #    #seq = map(lambda x:self.proproces.vocab_hind[x], tokens)
+            #    # Normalize seq to maxlen
+            #    X = []
+            #    X.append(seq)
+            #    print X
+            #    temp = sequence.pad_sequences(X, maxlen=self.maxlen)
+            #    #temp[0:len(seq)] = seq
+            #    print len(temp)
+            #    #temp = np.asarray(temp).reshape(128,)
+            #    print temp.shape
+            #    prob = model.predict_on_batch(temp)#, batch_size=1, verbose=0)
+            #    translated = self.decode(prob)
+            #    print("Tranlated is", translated)
+            #    print("Probabilities are", prob)
+            #    print("Shape of prob tensor is",prob.shape)
 
 if __name__ == "__main__":
-    seq2seq = seq2seq(32, 5000)
+    seq2seq = seq2seq(50, 5000)
     seq2seq.train_seq2seq()
